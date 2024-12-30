2b90fb80e30c195e8c1c6e73c28ad0268c8c8900
Zoran Cvijic <cvijiczoran4@gmail.com>
2024-12-30 14:32:03 +0100

    Created admin middleware

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+<h1>This is only for admin users</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::get('/test', function () {
     return view('test');
 })->middleware('auth')->name('test');
 
+Route::get('/admin', function () {
+    return view('admin');
+})->middleware(['auth', 'admin'])->name('admin');
+
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::get('/register', [AuthController::class, 'showRegistration'])->name('register');
 Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
