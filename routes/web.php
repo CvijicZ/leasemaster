3e47360c68296d30d0bd4080e7512f9c367603a3
Zoran Cvijic <cvijiczoran4@gmail.com>
2025-01-15 16:12:29 +0100

    Refactored routes for better readability

--- routes/web.php
+++ routes/web.php
@@ -11,53 +11,46 @@ Route::get('/', function () {
     return view('home', app(VehicleController::class)->index());
 })->name('home');
 
-Route::get('/contract/{id}', [ContractController::class, 'show'])->middleware('contract.owner')->name('contract.show');
+Route::get('/contract/{id}', [ContractController::class, 'show'])
+    ->middleware('contract.owner')
+    ->name('contract.show');
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/vehicles/{id}', [VehicleController::class, 'show'])->name('vehicles.show');
-    Route::view('/contractTest', 'user.create-contract');
     Route::get('/contract/{contract}', [ContractController::class, 'create'])->name('contract.create');
-
-
     Route::get('/lease/enquire', [ContractController::class, 'create'])->name('lease.create');
     Route::post('/lease/enquire', [ContractController::class, 'store'])->name('lease.store');
+    Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
 });
 
-Route::middleware(['admin'])
-    ->prefix('admin')
-    ->controller(UserController::class)
-    ->group(function () {
+Route::middleware(['admin'])->prefix('admin')->group(function () {
 
-        Route::get('/vehicles/create', [VehicleController::class, 'create'])->name('vehicles.create');
-        Route::post('/vehicles', [VehicleController::class, 'store'])->name('vehicles.store');
-        Route::delete('/vehicles/delete', [VehicleController::class, 'destroy'])->name('vehicles.delete');
-        Route::get('/contract/{id}', [ContractController::class, 'show'])->name('admin.contract.show');
+    Route::controller(VehicleController::class)->group(function () {
+        Route::get('/vehicles/create', 'create')->name('vehicles.create');
+        Route::post('/vehicles', 'store')->name('vehicles.store');
+        Route::delete('/vehicles/delete', 'destroy')->name('vehicles.delete');
 
-
-        Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
-
-        Route::get('/rentals', function () {
+        Route::get('/rentals', function () { // VehicleController index is used in multiple views, that's why this route is not simple route view
             return view('vehicles.index', app(VehicleController::class)->index());
         })->name('admin.rentals');
+    });
 
-        Route::get('/settings', function () {
-            return view('admin.settings');
-        })->name('admin.settings');
-
+    Route::controller(UserController::class)->group(function () {
         Route::get('/users', 'index')->name('admin.users');
         Route::get('/users/{user}/edit', 'edit')->name('admin.users.edit');
-
         Route::delete('/users/{user}', 'destroy')->name('admin.users.destroy');
-
         Route::put('/users/{user}', 'update')->name('admin.users.update');
         Route::put('/users/{user}/role', 'updateRole')->name('admin.users.updateRole');
     });
 
+    Route::get('/contract/{id}', [ContractController::class, 'show'])->name('admin.contract.show');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
+    Route::view('/settings', 'admin.settings')->name('admin.settings');
+});
+
 Route::controller(AuthController::class)->group(function () {
     Route::get('/login', 'showLogin')->name('login');
     Route::get('/register', 'showRegistration')->name('register');
-    Route::get('/logout', 'logout')->name('auth.logout');
-
     Route::post('/register', 'store')->name('auth.store');
     Route::post('/login', 'authenticate')->name('auth.login');
 });
